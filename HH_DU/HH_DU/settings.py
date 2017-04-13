ed2f6606d1bc713beb851f59a6769c6ca620b422
vasu-dev <vasu.singh002@gmail.com>
2017-04-13 15:15:07 +0530

    penutlimate_server

--- HH_DU/HH_DU/settings.py
+++ HH_DU/HH_DU/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'f9#cjk^$5pkp2(@^5)ma)y978(!)u#&m2y@g@)pz7(tw@f0ij*'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [u'7efdc023.ngrok.io']
+ALLOWED_HOSTS = [u'26d1bffa.ngrok.io']
 
 
 # Application definition
